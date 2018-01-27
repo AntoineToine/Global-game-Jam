9cac5e7e1e9259f3f3b20f70440e2074a1c5ff98
Gabriel Girard <gabriel.girard@epitech.eu>
2018-01-27 02:06:45 +0100

    blender

---
+++ GGJ/Assets/RandSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandSpawn : MonoBehaviour {
+    public Vector3 position;
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        int rand = Random.Range(-20, 20);
+        position.x = rand;
+        transform.position = position;
+    }
+}
